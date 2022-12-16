35684dee225fff304b0f0453f128d58bf3709843
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2022-12-16 17:36:22 -0500

    "funcionalidad inicial lista"

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const password2 = document.getElementById('password2');
 
 //Definición de funciones
 //Show inputs error message
+//Función para mostrar el mensaje de error
 function showError(input,message){
     const formControl = input.parentElement; //Captura el elemento padre, en este ejemplo es el div con clase form-control div
     formControl.className='form-control error' ;//Sobreescribimos la clase de este elemento con la del error
@@ -15,7 +16,20 @@ function showError(input,message){
     smallTag.innerText =message;
 }
 
+//Función para mostrar el mensaje de éxito
+function showSuccess(input){
+    const formControl = input.parentElement; //Captura el elemento padre, en este ejemplo es el div con clase form-control div
+    formControl.className='form-control success' ;//Sobreescribimos la clase de este elemento con la del error
 
+
+}
+
+
+//Función para validar el email
+function isValidEmail(email){
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 
 
 
@@ -24,6 +38,8 @@ function showError(input,message){
 form.addEventListener('submit', function(event){
     event.preventDefault();
     //console.log('submit');
+
+    //Sección para el username
     console.log(username.value); // capturamos aqui la variable del elementbyid y la propiedad exacta
 
     if(username.value===''){
@@ -32,4 +48,43 @@ form.addEventListener('submit', function(event){
     }else{
         showSuccess(username)
     }
+
+
+    //Sección para el email
+    console.log(email.value); // capturamos aqui la variable del elementbyid y la propiedad exacta
+
+    if(email.value===''){
+        showError(email,'Email is required'); //Llamamos una función que se va a llamar showError
+
+    }else if(!isValidEmail(email.value)){
+        showError(email,'Email is not valid'); //Llamamos una función que se va a llamar showError
+
+    }
+    else{
+        showSuccess(email)
+    }
+
+
+    //Sección para el password
+    console.log(password.value); // capturamos aqui la variable del elementbyid y la propiedad exacta
+
+    if(password.value===''){
+        showError(password,'password is required'); //Llamamos una función que se va a llamar showError
+
+    }else{
+        showSuccess(password)
+    }
+
+
+    //Sección para el password2
+    console.log(password2.value); // capturamos aqui la variable del elementbyid y la propiedad exacta
+
+    if(password2.value===''){
+        showError(password2,'Password again is required'); //Llamamos una función que se va a llamar showError
+
+    }else{
+        showSuccess(password2)
+    }
+
+
 })
